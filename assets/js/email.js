0c243c18117f4914c52d1d04c7de7abe6df8fb73
AudreyLL88 <a.lloancy@gmail.com>
2021-01-06 23:42:15 +0000

    emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,15 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail","audrey", {
-                                    "from_name": contactForm.name.value,
-                                    "from_email": contactForm.email.value,
-                                    "project_request": contactForm.projectsummary.value
-})
-.then(
-    function(response){
-        console.log("Success!", response);
-    },
-    function(error) {
-        console.log("FAILED", error);
-    });
-    return false;
+    emailjs.send("service_yzlka1p","audrey_contact", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
 }
